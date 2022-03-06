590cdaf2f1a77ab544ff816c4123929a2bfb0205
UroborosQ <uroborosq@ya.ru>
2022-03-06 21:49:43 +0300

    feat: parabola method added

--- Lab1/main.py
+++ Lab1/main.py
@@ -9,10 +9,17 @@ def investigated_function(x):
 if __name__ == "__main__":
     a, b, n = methods.dichotomy(investigated_function, 4, 6, 0.0005)
     print(a, b, n)
+    print(a-b)
 
     a, b, n = methods.golden_ratio(investigated_function, 4, 6, 0.0005)
     print(a, b, n)
+    print(a-b)
 
     a, b, n = methods.fibonacci(investigated_function, 4, 6, 0.0005)
     print(a, b, n)
+    print(a-b)
+
+    a, b, n = methods.parabola(investigated_function, 4, 6, 0.0005)
+    print(a, b, n)
+    print(a - b)
 
