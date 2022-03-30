4e2de82ac8147a61ac9c560a68247f571044856e
lttlmw <vvys83@gmail.com>
2022-03-30 21:20:34 +0300

    add: added tests

--- Lab1/main.py
+++ Lab1/main.py
@@ -6,24 +6,51 @@ def investigated_function(x):
     return m.sin(x) * pow(x, 3)
 
 
+def function1(x):
+    return 5 * m.cos(x) + pow(x, 2)
+
+
+def function2(x):
+    return x * x - x + 1
+
+
+def test(function, left, right, error):
+    print(left, ' ', right, ' ', error)
+    print("dichotomy")
+    a, b, n = methods.dichotomy(function, left, right, error)
+    print(a, b, n)
+
+    print("golden ratio")
+    a, b, n = methods.golden_ratio(function, left, right, error)
+    print(a, b, n)
+
+    print("fibonacci")
+    a, b, n = methods.fibonacci(function, left, right, error)
+    print(a, b, n)
+
+    print("parabola")
+    a, b, n = methods.parabola(function, left, right, error)
+    print(a, b, n)
+
+    print("brent")
+    a, b, n = methods.brent(function, left, right, error)
+    print(a, b, n)
+
+    print('\n')
+
+
 if __name__ == "__main__":
-    a, b, n = methods.dichotomy(investigated_function, 4, 6, '0.00000005')
-    print(a, b, n)
-    print(a-b)
+    test(investigated_function, 4, 6, '0.005')
+    test(investigated_function, 4, 6, '0.0005')
+    test(investigated_function, 4, 6, '0.00005')
 
-    a, b, n = methods.golden_ratio(investigated_function, 4, 6, '0.0005')
-    print(a, b, n)
-    print(a-b)
+    test(function1, 1, 4, '0.005')
+    test(function1, 1, 4, '0.0005')
+    test(function1, 1, 4, '0.00005')
 
-    a, b, n = methods.fibonacci(investigated_function, 4, 6, '0.0005')
-    print(a, b, n)
-    print(a-b)
+    test(function2, -1, 2, '0.005')
+    test(function2, -1, 2, '0.0005')
+    test(function2, -1, 2, '0.00005')
 
-    a, b, n = methods.parabola(investigated_function, 4, 6, '0.0005')
-    print(a, b, n)
-    print(a - b)
 
-    a, b, n = methods.brent(investigated_function, 4, 6, '0.0005')
-    print(a, b, n)
-    print(a - b)
 
