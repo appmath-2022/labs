106d459741b6ac785584b6aba67cb4d50fb4d4f0
ktg <optimys.prajm@mail.ru>
2022-12-22 00:57:08 +0300

    feat: add testing v and kernel

--- Lab8/functions.py
+++ Lab8/functions.py
@@ -6,8 +6,9 @@ import seaborn as sns
 
 
 def generate_data(size):
-    return pd.DataFrame(data={'x': [random.uniform(-1, 1) for i in range(size)],
+    data = pd.DataFrame(data={'x': [random.uniform(-1, 1) for i in range(size)],
                               'y': [random.uniform(-1, 1) for i in range(size)]})
+    return data.assign(ans=data.x ** 2 + data.y ** 2 < 0.25)
 
 
 def accuracy(true, pred):
@@ -43,4 +44,35 @@ def print_all_metrics(true, pred):
 def plot_data(data):
     plt.rcParams['axes.prop_cycle'] = plt.cycler(color=['#32a1ce', '#121212'])
     sns.scatterplot(x='x', y='y', hue='ans', data=data)
+    plt.show()
+
+
+def plot_decision_function(decision_function, X, Y, title=None):
+    xx, yy = np.meshgrid(np.linspace(-1, 1, 100), np.linspace(-1, 1, 100))
+    decision_result = decision_function(np.c_[xx.ravel(), yy.ravel()])
+    decision_result = decision_result.reshape(xx.shape)
+    plt.imshow(
+        decision_result,
+        interpolation='nearest',
+        extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+        aspect='auto',
+        origin='lower'
+    )
+
+    plt.contour(
+        xx,
+        yy,
+        decision_result,
+        levels=[0],
+    )
+
+    plt.scatter(
+        X['x'],
+        X['y'],
+        c=Y,
+        edgecolors='k'
+    )
+
+    plt.title(title)
+    plt.axis([-1, 1, -1, 1])
     plt.show()
--- Lab8/main.py
+++ Lab8/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import sklearn.model_selection
 from sklearn import svm
-
 import functions
 
 
@@ -10,7 +10,6 @@ def testing_data_size():
     accuracies, precisions, recalls, f1scores = [], [], [], []
     for i in test_range:
         data = functions.generate_data(i)
-        data = data.assign(ans=data.x ** 2 + data.y ** 2 < 0.25)
         X = data[['x', 'y']]
         y = data['ans']
         X_train, X_test, y_train, y_test = \
@@ -38,5 +37,60 @@ def testing_data_size():
     plt.show()
 
 
+def testing_kernels():
+    data = functions.generate_data(20000)
+    X = data[['x', 'y']]
+    y = data['ans']
+    X_train, X_test, y_train, y_test = \
+        sklearn.model_selection.train_test_split(X, y)
+    for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
+        clf = svm.NuSVC(gamma="auto", nu=0.09, kernel=kernel, degree=2)
+        clf.fit(X_train.values, y_train.values)
+        y_pred = clf.predict(X_test.values)
+        print(kernel)
+        functions.print_all_metrics(y_test, y_pred)
+        functions.plot_decision_function(clf.decision_function, X_test, y_pred, title=kernel)
+
+
+def testing_lower_bound():
+    data = functions.generate_data(20000)
+    X = data[['x', 'y']]
+    y = data['ans']
+    X_train, X_test, y_train, y_test = \
+        sklearn.model_selection.train_test_split(X, y)
+    test_range = np.arange(0.01, 1.01, 0.01)
+    accuracies, precisions, recalls, f1scores = [], [], [], []
+    for nu in test_range:
+        try:
+            clf = svm.NuSVC(gamma="auto", nu=nu, kernel='rbf')
+            clf.fit(X_train.values, y_train.values)
+            y_pred = clf.predict(X_test.values)
+            accuracies.append(functions.accuracy(y_test, y_pred))
+            precisions.append(functions.precision(y_test, y_pred))
+            recalls.append(functions.recall(y_test, y_pred))
+            f1scores.append(functions.f_score(y_test, y_pred))
+        except ValueError:
+            accuracies.append(accuracies[-1])
+            precisions.append(precisions[-1])
+            recalls.append(recalls[-1])
+            f1scores.append(f1scores[-1])
+            pass
+
+    plt.plot(test_range, accuracies)
+    plt.legend(["accuracy"])
+    plt.show()
+    plt.plot(test_range, precisions)
+    plt.legend(["precision"])
+    plt.show()
+    plt.plot(test_range, recalls)
+    plt.legend(["recall"])
+    plt.show()
+    plt.plot(test_range, f1scores)
+    plt.legend(["f1 score"])
+    plt.show()
+
+
 if __name__ == '__main__':
     testing_data_size()
+    testing_kernels()
+    testing_lower_bound()
